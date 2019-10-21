a460216997bff3a6726b4f8b458feba6a054a315
CallSayDifferent <56811933+CallSayDifferent@users.noreply.github.com>
2019-10-21 20:17:37 +0700

    Update MainActivity.java

--- app/src/main/java/com/azhar/dzikir/MainActivity.java
+++ app/src/main/java/com/azhar/dzikir/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         getSupportActionBar().setDisplayShowEnabled(true);
         dzikir = (TextView) findViewById(R.id.dzikir);
         vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
     }
